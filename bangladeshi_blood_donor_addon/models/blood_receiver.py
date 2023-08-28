3a29ed80609b69a719bab75cec11459425610add
Azizullah Tanim <42853370+tanim913@users.noreply.github.com>
2023-08-29 01:51:08 +0600

    [ADD] added blood_receiver.py

---
+++ bangladeshi_blood_donor_addon/models/blood_receiver.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api, _
+
+
+class BloodReceiver(models.Model):
+    _name = "blood.receiver"
+
+    blood_donor_id = fields.Many2one('blood.donor', string='Blood Donor')
