404f6070412a2c042ec01186e4621fda397927ed
Azizullah Tanim <42853370+tanim913@users.noreply.github.com>
2023-08-29 02:42:27 +0600

    [update] made blood_donor_id required

--- bangladeshi_blood_donor_addon/models/blood_receiver.py
+++ bangladeshi_blood_donor_addon/models/blood_receiver.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api, _
 class BloodReceiver(models.Model):
     _name = "blood.receiver"
 
-    blood_donor_id = fields.Many2one('blood.donor', string='Blood Donor')
+    blood_donor_id = fields.Many2one('blood.donor', string='Blood Donor', required=True)
